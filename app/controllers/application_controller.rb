0408ff738dac1ddde3acc6e7c105c826d61a09aa
JD-Hiudt <jacob.d.hiudt@gmail.com>
2017-07-24 15:24:56 -0500

    add username to db, add strong params, protect profile path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # call the config params
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #protect db
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remeber_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password, :password_confirmation, :remeber_me]) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :remeber_me])
+  end
+
 end
